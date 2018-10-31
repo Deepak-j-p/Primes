5c7408f5169a98b891e4707fbd131cdd84add2e2
Deepak-j-p <deepakjayaprakash007@gmail.com>
2018-10-31 09:22:09 +0530

    Improved the time complexity
    Improved the time complexity for the following cpp file

--- promus.cpp
+++ promus.cpp
@@ -5,15 +5,15 @@ using namespace std;
 int main()
 {
     int number,i, value;
-    cout<<"Enter Number = ";
+    cout<<"Enter Number = \n";
     cin>>number;
     value=0;
-    for (i=1;i<=number;i++){
+    for (i=2;i<=number/2;i++){
         if (number%i==0) //Condition to find number who while it modulo with i the value is 0 (zero)
-        value = value +i;
+          value=1;
         }
         //Conditions to determining prime number or not
-        if(value==(number+1)){
+        if(value==0){
             cout<<"PRIMES";
         }
         else{
